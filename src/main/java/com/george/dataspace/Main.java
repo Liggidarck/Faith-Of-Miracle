f8b753331ae9c7d6019dfa210216c87962a6441c
george <georgyfilatov@yandex.ru>
2023-02-05 21:53:05 +0300

    init commit

---
+++ src/main/java/com/george/dataspace/Main.java
@@ -0,0 +1,10 @@
+package com.george.dataspace;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Приветствуем тебя, странник!\nТы заглянул в в потрясающий мир" +
+                "в котором ты можешь стать кем угодно. ");
+
+    }
+}
